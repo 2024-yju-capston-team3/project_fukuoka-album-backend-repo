0286234fb6a9537ed3024df95204bfa9ef80b866
Z00One <9000248@g.yju.ac.kr>
2023-10-12 19:59:34 +0900

    feat: Implement refresh token and verify token endpoint

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,15 +1,27 @@
 import express, { Request, Response } from "express";
-import { signIn } from "../controller/auth"
+import { refresh, signIn } from "../controller/auth"
+import { authMiddleware } from "../middleware/authMiddleware";
+
 const router = express.Router();
 
 export const Auth = () => {
-  router.get('/', async (req: Request, res: Response) => {
-    const credential = req.headers.authorization;
+  router.post('/', async (req: Request, res: Response) => {
+    const credential = req.body.userInfo;
 
-    const user = await signIn(credential);
+    const result = await signIn(credential);
 
-    res.json(user);
-  })
+    res.status(result.statusCode).json(result);
+  });
+
+  router.get('/verify', authMiddleware(), async (req: Request, res: Response) => {
+    res.status(200).json("OK");
+  });
+
+  router.post("/refresh", authMiddleware(true), async (req: Request, res: Response) => {
+    const result = await refresh(req);
+
+    res.status(result.statusCode).json(result.data);
+  });
 
   return router;
 }
