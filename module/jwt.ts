a1ceb313181722419caf193fcf4338fb8ad6d741
Z00One <9000248@g.yju.ac.kr>
2023-10-14 20:12:36 +0900

    fix(module/jwt): Fix token verification error

--- module/jwt.ts
+++ module/jwt.ts
@@ -1,16 +1,17 @@
-import jwt from 'jsonwebtoken';
+import jwt, { TokenExpiredError } from "jsonwebtoken";
 import {
   AccessSecretKey,
   RefreshSecretKey,
   AccessTokenoption,
   RefreshTokenoption,
-} from '../config/secretkey';
+} from "../config/secretkey";
+import { HttpException } from "../util/response";
 
 export const JWT = {
   /** 토큰 발급 */
   sign: async (email: string, isRefresh?: boolean) => {
     const payload = {
-      email
+      email,
     };
 
     if (isRefresh) {
@@ -21,7 +22,7 @@ export const JWT = {
 
     const token = {
       accessToken: jwt.sign(payload, AccessSecretKey, AccessTokenoption),
-      refreshToken: jwt.sign(payload, RefreshSecretKey, RefreshTokenoption)
+      refreshToken: jwt.sign(payload, RefreshSecretKey, RefreshTokenoption),
     };
 
     return token;
@@ -29,16 +30,18 @@ export const JWT = {
 
   /** 토큰 인증 */
   verify: (token: string, isRefresh?: boolean) => {
-    let isVerify = true;
-    const secretKey = isRefresh ? RefreshSecretKey : AccessSecretKey 
-    
+    const secretKey = isRefresh ? RefreshSecretKey : AccessSecretKey;
+
     try {
       jwt.verify(token, secretKey);
     } catch (error) {
       console.log(error);
-      isVerify = false;
+      if (error instanceof TokenExpiredError) {
+        throw new HttpException(410, "유효 기간이 지난 토큰입니다.");
+      }
+      throw new HttpException(401, "권한이 없습니다.");
     }
 
-    return isVerify;
-  }
-}
+    return true;
+  },
+};
