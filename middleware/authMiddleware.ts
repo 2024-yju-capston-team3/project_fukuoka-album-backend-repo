421cd681e6f433c196772c2a1094899129d76ba2
Z00One <9000248@g.yju.ac.kr>
2023-10-14 20:28:18 +0900

    refactor: Refactor authMiddleware function

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,22 +1,19 @@
-import { Request, Response, NextFunction } from 'express';
-import { asyncWrapper } from '../util/asyncWrapper';
-import { HttpException } from '../util/response';
-import { verify } from '../controller/auth';
+import { Request, Response, NextFunction } from "express";
+import { asyncWrapper } from "../util/asyncWrapper";
+import { HttpException } from "../util/response";
+import { verify } from "../controller/auth";
 
 export const authMiddleware = (isRefresh?: boolean) =>
   asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
-    const token = isRefresh ? req.body?.authorization : req.headers?.authorization;
-    
+    const token = isRefresh
+      ? req.body?.authorization
+      : req.headers?.authorization;
+
     if (!token) {
-      throw new HttpException(422, '인증할 수 없습니다.');
+      throw new HttpException(422, "인증할 수 없습니다.");
     }
-    
-    const isVerify = verify(token, isRefresh);
-    
-    if (!isVerify) {
-      throw new HttpException(401, '사용자 권한이 없습니다.');
-    }
+
+    verify(token, isRefresh);
 
     next();
   });
-;
