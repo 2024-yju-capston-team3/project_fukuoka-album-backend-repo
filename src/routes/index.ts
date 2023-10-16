1949811a344676899c884785fa2542b5fb5a45af
Z00One <9000248@g.yju.ac.kr>
2023-10-16 23:44:20 +0900

    feat: add endpoint for upload

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import { Auth } from "./auth";
+import { Upload } from "./upload";
 
 export const routes = (app: Express) => {
 	app.use("/auth", Auth());
+	app.use("/upload", Upload());
 };
