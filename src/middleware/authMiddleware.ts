fe39c614c57c283538f9285587a90cf5f8c0ac86
Z00One <9000248@g.yju.ac.kr>
2023-10-21 16:50:02 +0900

    refactor: Separate accessMiddleware and refreshMiddleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,19 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import { asyncWrapper } from "../util/asyncWrapper";
 import { UnprocessableEntityException } from "../util/exception";
-import { verify } from "../controller/authController";
+import { JWT } from "../module/jwt";
 
-export const authMiddleware = (isRefresh?: boolean) =>
-	asyncWrapper(async (req: Request, res: Response, next: NextFunction) => {
-		const token = isRefresh
-			? req.body?.authorization
-			: req.headers?.authorization;
+export const accessMiddleware = asyncWrapper(
+	(req: Request, res: Response, next: NextFunction) => {
+		const token = req.headers?.authorization;
 
 		if (!token) {
 			throw new UnprocessableEntityException("토큰이 없습니다.");
 		}
 
-		verify(token, isRefresh);
+		JWT.verify(token);
 
 		next();
-	});
+	}
+);
+
+export const refreshMiddleware = asyncWrapper(
+	(req: Request, res: Response, next: NextFunction) => {
+		const token = req.body?.authorization;
+
+		if (!token) {
+			throw new UnprocessableEntityException("토큰이 없습니다.");
+		}
+
+		JWT.refreshVerify(token);
+
+		next();
+	}
+);
